12f0fcd9e6923619f3e797c4f106d16fc68209c0
Lalere <51694924+Lalere@users.noreply.github.com>
2022-05-19 10:50:21 +0200

    Update AntennaService.java

--- src/main/java/org/sebi/AntennaService.java
+++ src/main/java/org/sebi/AntennaService.java
@@ -10,7 +10,7 @@ public class AntennaService {
     public List<Antenna> getAntennas(){
         Antenna antenna = new Antenna(46.928881, 2.409900);
         Antenna antenna2 = new Antenna(44.938881, 1.419900);
-        Antenna antenna3 = new Antenna(45.2, 2.2900);
+        Antenna antenna3 = new Antenna(45.3, 2.2900);
         return List.of(antenna,antenna2,antenna3);
     }
 }
